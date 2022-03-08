298076e0866a966e4c41d9b3280a90527789e824
jeremyandes <jeremyandes25@gmail.com>
2022-03-08 19:17:53 -0300

    15. Objetos, arreglos e interfaces

---
+++ index.js
@@ -0,0 +1,8 @@
+// let habilidades: (boolean | string | number)[] = []
+var personaje = {
+    nombre: 'Superman',
+    hp: 100,
+    habilidades: ['Volar', 'Rayos X', 'Fuerza']
+};
+personaje.ciudadNatal = 'Asdasd';
+console.table(personaje);
