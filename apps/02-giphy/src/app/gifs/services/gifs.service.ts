697a6596e82a0907dd8f6e6e71a4f08508b334cb
jeremyandes <jeremyandes25@gmail.com>
2022-03-12 12:53:32 -0300

    88. LocalStorage

--- apps/02-giphy/src/app/gifs/services/gifs.service.ts
+++ apps/02-giphy/src/app/gifs/services/gifs.service.ts
@@ -6,16 +6,20 @@ import { Gif, SearchGifsResponse } from '../interfaces/gifs.interface';
   providedIn: 'root',
 })
 export class GifsService {
-  private _history: string[] = [];
-  private apiKey: string = 'h0lXkO2rN89vY3PLpQp23Se8J0su2WsP';
   private protocol: string = 'https';
   private baseURL: string = 'api.giphy.com/v1/gifs';
+  private apiKey: string = 'h0lXkO2rN89vY3PLpQp23Se8J0su2WsP';
+
   private searchEndpoint: string = 'search';
+
+  private _history: string[] = [];
 
   public results: Gif[] = [];
 
   constructor(private http: HttpClient) {
     console.log('GifsService constructor');
+
+    this._history = JSON.parse(localStorage.getItem('history')!) || [];
   }
 
   get history() {
@@ -30,9 +34,12 @@ export class GifsService {
     this._history.unshift(query);
     this._history = this._history.splice(0, 10);
 
+    localStorage.setItem('history', JSON.stringify(this._history));
+
     const httpGet = this.http.get<SearchGifsResponse>(
       `${this.protocol}://${this.baseURL}/${this.searchEndpoint}?api_key=${this.apiKey}&q=${query}&limit=10`
     );
+
     httpGet.subscribe((resp) => {
       this.results = resp.data;
       console.log(this.results);
