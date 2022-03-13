7fd31bb73daa4bb6fbf541ae83f70906ac0487d9
jeremyandes <jeremyandes25@gmail.com>
2022-03-13 20:02:06 -0300

    104. Componente para buscar por país

---
+++ apps/03-paises/src/app/pais/pages/por-pais/por-pais.component.ts
@@ -0,0 +1,16 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-por-pais',
+  templateUrl: './por-pais.component.html',
+  styles: [],
+})
+export class PorPaisComponent {
+  pais: string = 'Hola mundo';
+
+  constructor() {}
+
+  buscar() {
+    console.log(this.pais);
+  }
+}
