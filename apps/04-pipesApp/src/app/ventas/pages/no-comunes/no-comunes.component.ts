a63b60c73b3bbacecb1fa5a5aa65485e44bfeb49
jeremyandes <jeremyandes25@gmail.com>
2022-05-07 20:40:21 -0300

    Seccion 10 Finalizada > 156. AsyncPipe

--- apps/04-pipesApp/src/app/ventas/pages/no-comunes/no-comunes.component.ts
+++ apps/04-pipesApp/src/app/ventas/pages/no-comunes/no-comunes.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import { interval, Observable } from 'rxjs';
 
 @Component({
   selector: 'app-no-comunes',
@@ -8,26 +9,34 @@ import { Component } from '@angular/core';
 })
 export class NoComunesComponent {
 
+  collapsed: boolean = true;
+
   nombre: string = 'Jeremy';
   genero: string = 'male';
 
-  selectMap = {
+  selectMap: any = {
     'male': 'bienvenido',
     'female': 'bienvenida',
   }
 
   clientes: string[] = ['Laura', 'Pedro', 'Juan', 'Juana', 'Jeremy', 'Camila', 'Thiago'];
-  clientesMap = {
+  clientesMap: any = {
     '=0': 'no tenemos clientes esperando',
     '=1': 'tenemos 1 cliente esperando',
     'other': `tenemos ${this.clientes.length} clientes esperando`,
   }
 
-  persona = {
+  persona: object = {
     nombre: 'Jeremy',
     edad: 23,
     localidad: 'Mar del Plata, Argentina'
   }
+
+  miObservable: Observable<number> = interval(1000);
+
+  valorPromesa = new Promise((resolve) => {
+    setTimeout(() => resolve('Tenemos data de promesa'), 3500)
+  })
 
   public cambiarGenero(): void {
     if (this.genero == 'male') {
