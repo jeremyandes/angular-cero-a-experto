43d357b306324715c8403fb25f64bf4dc0147128
jeremyandes <jeremyandes25@gmail.com>
2022-05-07 18:45:29 -0300

    151. I18nPluralPipe

--- apps/04-pipesApp/src/app/ventas/pages/no-comunes/no-comunes.component.ts
+++ apps/04-pipesApp/src/app/ventas/pages/no-comunes/no-comunes.component.ts
@@ -16,4 +16,21 @@ export class NoComunesComponent {
     'female': 'bienvenida',
   }
 
+  clientes: string[] = [];
+  clientesMap = {
+    '=0': 'no tenemos clientes esperando',
+    '=1': 'tenemos 1 cliente esperando',
+    'other': `tenemos ${this.clientes.length} clientes esperando`,
+  }
+
+  public cambiarGenero(): void {
+    this.genero == 'male'
+      ? this.genero = 'female'
+      : this.genero = 'male'
+  }
+
+  public agregarcliente(): void {
+    this.clientes.push(`Cliente ${this.clientes.length + 1}`);
+  }
+
 }
