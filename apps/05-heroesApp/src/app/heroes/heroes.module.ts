c2b0791f26d28f073c094d95e5b566c947635260
jeremyandes <jeremyandes25@gmail.com>
2022-05-08 12:39:57 -0300

    185. Material Sidenav, Toolbar e iconos

--- apps/05-heroesApp/src/app/heroes/heroes.module.ts
+++ apps/05-heroesApp/src/app/heroes/heroes.module.ts
@@ -7,6 +7,7 @@ import { AddComponent } from './pages/add/add.component';
 import { HeroeComponent } from './pages/heroe/heroe.component';
 import { HomeComponent } from './pages/home/home.component';
 import { ListComponent } from './pages/list/list.component';
+import { MaterialModule } from '../shared/material/material.module';
 import { SearchComponent } from './pages/search/search.component';
 
 
@@ -23,6 +24,7 @@ import { SearchComponent } from './pages/search/search.component';
     CommonModule,
     FlexLayoutModule,
     HeroesRoutingModule,
+    MaterialModule,
   ]
 })
 export class HeroesModule { }
