2f9c592380ee4e76585392d59d869ac3421af07a
jeremyandes <jeremyandes25@gmail.com>
2022-05-08 19:38:09 -0300

    197. Variables de entorno

--- apps/05-heroesApp/src/app/heroes/services/heroes.service.ts
+++ apps/05-heroesApp/src/app/heroes/services/heroes.service.ts
@@ -1,13 +1,14 @@
 import { HttpClient } from "@angular/common/http";
 import { Injectable } from "@angular/core";
 import { Observable } from "rxjs";
+import { environment } from "src/environments/environment";
 import { Heroe } from "../interfaces/heroe.interface";
 
 @Injectable({
     providedIn: 'root'
 })
 export class HeroesService {
-    url: string = 'http://localhost:3000'
+    url: string = environment.baseUrl;
 
     constructor(private http: HttpClient) { }
 
