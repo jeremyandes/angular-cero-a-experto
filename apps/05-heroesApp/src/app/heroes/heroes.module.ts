eb1976d8f2a054a26f01828407b76cffee5962b9
jeremyandes <jeremyandes25@gmail.com>
2022-05-08 11:48:38 -0300

    178. Resolución de la tarea - Rujas Hijas de Héroes

--- apps/05-heroesApp/src/app/heroes/heroes.module.ts
+++ apps/05-heroesApp/src/app/heroes/heroes.module.ts
@@ -1,11 +1,12 @@
-import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
+import { HeroesRoutingModule } from './heroes-routing.module';
+import { NgModule } from '@angular/core';
+
 import { AddComponent } from './pages/add/add.component';
-import { SearchComponent } from './pages/search/search.component';
 import { HeroeComponent } from './pages/heroe/heroe.component';
 import { HomeComponent } from './pages/home/home.component';
 import { ListComponent } from './pages/list/list.component';
-import { HeroesRoutingModule } from './heroes-routing.module';
+import { SearchComponent } from './pages/search/search.component';
 
 
 
