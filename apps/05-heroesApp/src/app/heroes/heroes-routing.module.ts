fa31eda1d4a9434ec649b283239ea1a8f6b48b30
jeremyandes <jeremyandes25@gmail.com>
2022-05-08 11:56:10 -0300

    179. Mostrar Rutas Hijas - Segundo RouterOutlet

--- apps/05-heroesApp/src/app/heroes/heroes-routing.module.ts
+++ apps/05-heroesApp/src/app/heroes/heroes-routing.module.ts
@@ -5,12 +5,14 @@ import { RouterModule, Routes } from '@angular/router';
 
 import { AddComponent } from './pages/add/add.component';
 import { HeroeComponent } from './pages/heroe/heroe.component';
+import { HomeComponent } from './pages/home/home.component';
 import { ListComponent } from './pages/list/list.component';
 import { SearchComponent } from './pages/search/search.component';
 
 const routes: Routes = [
   {
     path: '',
+    component: HomeComponent,
     children: [
       {
         path: 'list',
