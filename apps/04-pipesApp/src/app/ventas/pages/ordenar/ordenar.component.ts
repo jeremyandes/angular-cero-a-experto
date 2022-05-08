f410a22cd772139e2f6c2571604dc54b0e99143c
jeremyandes <jeremyandes25@gmail.com>
2022-05-07 21:32:11 -0300

    164. Llenar un PrimeTable con data

--- apps/04-pipesApp/src/app/ventas/pages/ordenar/ordenar.component.ts
+++ apps/04-pipesApp/src/app/ventas/pages/ordenar/ordenar.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import { Color, Heroe } from '../../interfaces/heroe.interface';
 
 @Component({
   selector: 'app-ordenar',
@@ -8,6 +9,33 @@ import { Component } from '@angular/core';
 })
 export class OrdenarComponent {
   toggle: boolean = false;
+  heroes: Heroe[] = [
+    {
+      nombre: 'Superman',
+      vuela: true,
+      color: Color.azul
+    },
+    {
+      nombre: 'Batman',
+      vuela: false,
+      color: Color.negro
+    },
+    {
+      nombre: 'Robin',
+      vuela: false,
+      color: Color.amarillo
+    },
+    {
+      nombre: 'Daredevil',
+      vuela: false,
+      color: Color.rojo
+    },
+    {
+      nombre: 'Linterna Verde',
+      vuela: false,
+      color: Color.verde
+    },
+  ]
 
   mayusculasToggle() {
     this.toggle = this.toggle ? false : true;
