7ed0582498c1b6452eb681980a76c7489e7e9051
jeremyandes <jeremyandes25@gmail.com>
2022-05-07 21:09:57 -0300

    162. Valor y argumentos a los pipes personalizados

--- apps/04-pipesApp/src/app/ventas/pages/ordenar/ordenar.component.ts
+++ apps/04-pipesApp/src/app/ventas/pages/ordenar/ordenar.component.ts
@@ -1,4 +1,4 @@
-import { Component, OnInit } from '@angular/core';
+import { Component } from '@angular/core';
 
 @Component({
   selector: 'app-ordenar',
@@ -6,11 +6,11 @@ import { Component, OnInit } from '@angular/core';
   styles: [
   ]
 })
-export class OrdenarComponent implements OnInit {
+export class OrdenarComponent {
+  toggle: boolean = false;
 
-  constructor() { }
-
-  ngOnInit(): void {
+  mayusculasToggle() {
+    this.toggle = this.toggle ? false : true;
   }
 
 }
