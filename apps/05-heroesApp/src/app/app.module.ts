1d3c54b728111290d8e4b94e3b5d6e1af735116f
jeremyandes <jeremyandes25@gmail.com>
2022-05-08 10:59:11 -0300

    174. Módulos y componentes iniciales

--- apps/05-heroesApp/src/app/app.module.ts
+++ apps/05-heroesApp/src/app/app.module.ts
@@ -1,17 +1,17 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
-import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { NotFoundComponent } from './shared/error/not-found/not-found.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    NotFoundComponent
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule,
     BrowserAnimationsModule
   ],
   providers: [],
