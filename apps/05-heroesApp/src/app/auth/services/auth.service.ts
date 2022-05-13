1d23aaabe999e12c6b7acc08a79935305a4e7ae2
jeremyandes <jeremyandes25@gmail.com>
2022-05-12 21:21:20 -0300

    223. Mostrar la información del usuario activo

--- apps/05-heroesApp/src/app/auth/services/auth.service.ts
+++ apps/05-heroesApp/src/app/auth/services/auth.service.ts
@@ -2,16 +2,23 @@ import { Injectable } from '@angular/core';
 import { HttpClient } from '@angular/common/http'
 import { environment } from 'src/environments/environment';
 import { Auth } from '../interfaces/auth.interface';
+import { tap } from 'rxjs/operators';
 
 @Injectable({
   providedIn: 'root'
 })
 export class AuthService {
   private baseUrl: string = environment.baseUrl;
+  private _auth: Auth | undefined;
+
+  public get auth() { return { ...this._auth! }; }
 
   constructor(private http: HttpClient) { }
 
   login() {
-    return this.http.get<Auth>(`${this.baseUrl}/users/15`);
+    return this.http.get<Auth>(`${this.baseUrl}/users/1`)
+      .pipe(
+        tap(auth => this._auth = auth)
+      );
   }
 }
--- apps/05-heroesApp/src/app/heroes/pages/home/home.component.ts
+++ apps/05-heroesApp/src/app/heroes/pages/home/home.component.ts
@@ -1,5 +1,7 @@
 import { Component, OnInit } from '@angular/core';
 import { Router } from '@angular/router';
+import { Auth } from 'src/app/auth/interfaces/auth.interface';
+import { AuthService } from 'src/app/auth/services/auth.service';
 
 @Component({
   selector: 'app-home',
@@ -12,7 +14,12 @@ import { Router } from '@angular/router';
 })
 export class HomeComponent implements OnInit {
 
-  constructor(private router: Router) { }
+  constructor(
+    private router: Router,
+    private authService: AuthService,
+  ) { }
+
+  public get auth(): Auth { return this.authService.auth; }
 
   ngOnInit(): void {
   }
