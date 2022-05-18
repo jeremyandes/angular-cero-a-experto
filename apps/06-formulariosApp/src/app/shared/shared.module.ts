26bdf7592c6f25868c7075e0db6aef090f67557f
jeremyandes <jeremyandes25@gmail.com>
2022-05-18 12:47:57 -0300

    233. SideMenu

--- apps/06-formulariosApp/src/app/shared/shared.module.ts
+++ apps/06-formulariosApp/src/app/shared/shared.module.ts
@@ -1,6 +1,7 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { SidemenuComponent } from './sidemenu/sidemenu.component';
+import { RouterModule } from '@angular/router';
 
 
 
@@ -10,6 +11,7 @@ import { SidemenuComponent } from './sidemenu/sidemenu.component';
   ],
   imports: [
     CommonModule,
+    RouterModule,
   ],
   exports: [
     SidemenuComponent,
