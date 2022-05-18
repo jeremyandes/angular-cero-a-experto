adc849da1ef877c6bea54eb5a349be814f9a8adb
jeremyandes <jeremyandes25@gmail.com>
2022-05-18 11:45:16 -0300

    Seccion 15 Finalizada > 226. Mantener la sesión del usuario

--- apps/05-heroesApp/src/app/auth/guards/auth.guard.ts
+++ apps/05-heroesApp/src/app/auth/guards/auth.guard.ts
@@ -1,6 +1,7 @@
 import { Injectable } from '@angular/core';
-import { ActivatedRouteSnapshot, CanActivate, CanLoad, Route, RouterStateSnapshot, UrlSegment, UrlTree } from '@angular/router';
+import { ActivatedRouteSnapshot, CanActivate, CanLoad, Route, Router, RouterStateSnapshot, UrlSegment, UrlTree } from '@angular/router';
 import { Observable } from 'rxjs';
+import { tap } from 'rxjs/operators';
 import { AuthService } from '../services/auth.service';
 
 @Injectable({
@@ -10,31 +11,47 @@ export class AuthGuard implements CanActivate, CanLoad {
 
   constructor(
     private authService: AuthService,
+    private router: Router,
   ) { }
 
   canActivate(
     route: ActivatedRouteSnapshot,
     state: RouterStateSnapshot): Observable<boolean> | Promise<boolean> | boolean {
 
-    if (this.authService.auth.id) { return true; }
+    // if (this.authService.auth.id) { return true; }
 
-    console.log('canActivate', false);
-    console.log(route);
-    console.log(state);
+    // console.log('canActivate', false);
+    // console.log(route);
+    // console.log(state);
 
-    return false;
+    // return false;
+
+    return this.authService.checkToken()
+      .pipe(
+        tap(isAuthenticated => !isAuthenticated
+          ? this.router.navigate(['/auth'])
+          : console.log('Login success'))
+      );
+
   }
 
   canLoad(
     route: Route,
     segments: UrlSegment[]): Observable<boolean> | Promise<boolean> | boolean {
 
-    if (this.authService.auth.id) { return true; }
+    // if (this.authService.auth.id) { return true; }
 
-    console.log('canLoad', false);
-    console.log(route);
-    console.log(segments);
+    // console.log('canLoad', false);
+    // console.log(route);
+    // console.log(segments);
 
-    return false;
+    // return false;
+
+    return this.authService.checkToken().pipe(
+      tap(isAuthenticated => !isAuthenticated
+        ? this.router.navigate(['/auth'])
+        : console.log('Login success'))
+    );
+
   }
 }
--- apps/05-heroesApp/src/app/auth/services/auth.service.ts
+++ apps/05-heroesApp/src/app/auth/services/auth.service.ts
@@ -2,7 +2,8 @@ import { Injectable } from '@angular/core';
 import { HttpClient } from '@angular/common/http'
 import { environment } from 'src/environments/environment';
 import { Auth } from '../interfaces/auth.interface';
-import { tap } from 'rxjs/operators';
+import { map, tap } from 'rxjs/operators';
+import { Observable, of } from 'rxjs';
 
 @Injectable({
   providedIn: 'root'
@@ -14,6 +15,18 @@ export class AuthService {
   public get auth() { return { ...this._auth! }; }
 
   constructor(private http: HttpClient) { }
+
+  checkToken(): Observable<boolean> {
+    return !localStorage.getItem('idUser')
+      ? of(false)
+      : this.http.get<Auth>(`${this.baseUrl}/users/1`)
+        .pipe(
+          map(auth => {
+            this._auth = auth;
+            return true;
+          })
+        )
+  }
 
   login() {
     return this.http.get<Auth>(`${this.baseUrl}/users/1`)
