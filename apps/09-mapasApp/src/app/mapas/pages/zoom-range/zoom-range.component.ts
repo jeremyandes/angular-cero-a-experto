22ca74e2a7bdfe771e2460b3cd1eb7b63e28c55d
jeremyandes <jeremyandes25@gmail.com>
2022-06-05 22:00:53 -0300

    316. Obtener las coordenadas centrales del mapa

--- apps/09-mapasApp/src/app/mapas/pages/zoom-range/zoom-range.component.ts
+++ apps/09-mapasApp/src/app/mapas/pages/zoom-range/zoom-range.component.ts
@@ -1,4 +1,4 @@
-import { AfterViewInit, Component, ElementRef, ViewChild } from '@angular/core';
+import { AfterViewInit, Component, ElementRef, OnDestroy, ViewChild } from '@angular/core';
 import * as mapboxgl from 'mapbox-gl';
 
 @Component({
@@ -6,38 +6,50 @@ import * as mapboxgl from 'mapbox-gl';
   templateUrl: './zoom-range.component.html',
   styleUrls: ['./zoom-range.styles.scss'],
 })
-export class ZoomRangeComponent implements AfterViewInit {
+export class ZoomRangeComponent implements AfterViewInit, OnDestroy {
   private map!: mapboxgl.Map;
 
   @ViewChild('mapa') mapContainer!: ElementRef;
-  zoomLevel: number = 17;
+  zoomLevel: number = 11;
+  centerMap: [number, number] = [-57.53212015934875, -38.02606601755031];
 
   constructor() { }
+
+  ngOnDestroy(): void {
+    this.map.off('zoom', () => { })
+    this.map.off('zoomend', () => { })
+    this.map.off('move', () => { })
+  }
 
   ngAfterViewInit(): void {
     this.map = new mapboxgl.Map({
       container: this.mapContainer.nativeElement,
       style: 'mapbox://styles/mapbox/streets-v11',
-      center: [-57.53212015934875, -38.02606601755031],
+      center: this.centerMap,
       zoom: this.zoomLevel,
     });
 
     this.map.on('zoom', () => {
       this.zoomLevel = this.map.getZoom();
     });
+
     this.map.on('zoomend', () => {
       if (this.map.getZoom() > 18) { this.map.zoomTo(18); }
+    });
+
+    this.map.on('move', (event) => {
+      const { target } = event;
+      const { lng, lat } = target.getCenter();
+      this.centerMap = [lng, lat];
     });
   }
 
   zoomIn() {
     this.map.zoomIn();
-    console.log('zoomIn', this.mapContainer);
     this.zoomLevel = this.map.getZoom();
   }
   zoomOut() {
     this.map.zoomOut();
-    console.log('zoomOut', this.mapContainer);
     this.zoomLevel = this.map.getZoom();
   }
 
