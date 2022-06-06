b7303cf332738ea973a134cda398864ab580229a
jeremyandes <jeremyandes25@gmail.com>
2022-06-05 21:01:19 -0300

    312. Controlar el objeto del mapa - ZoomIn y ZoomOut

--- apps/09-mapasApp/src/app/mapas/pages/zoom-range/zoom-range.component.ts
+++ apps/09-mapasApp/src/app/mapas/pages/zoom-range/zoom-range.component.ts
@@ -1,41 +1,34 @@
-import { Component, OnInit } from '@angular/core';
+import { AfterViewInit, Component, ElementRef, ViewChild } from '@angular/core';
 import * as mapboxgl from 'mapbox-gl';
 
 @Component({
   selector: 'app-zoom-range',
   templateUrl: './zoom-range.component.html',
-  styles: [`
-    .mapa-container {
-      width: 100%;
-      height: 100%;
-    }
-    .row {
-      background-color: white;
-      z-index: 999;
-      position: fixed;
-      bottom: 50px;
-      left: 50px;
-      padding: 10px;
-      border-radius: 12px;
-
-      & .form-label {
-        margin: 0 !important;
-        padding: 5px;
-      }
-    }
-  `]
+  styleUrls: ['./zoom-range.styles.scss'],
 })
-export class ZoomRangeComponent implements OnInit {
+export class ZoomRangeComponent implements AfterViewInit {
+  private map!: mapboxgl.Map;
+
+  @ViewChild('mapa') mapContainer!: ElementRef;
 
   constructor() { }
 
-  ngOnInit(): void {
-    const map = new mapboxgl.Map({
-      container: 'mapa',
+  ngAfterViewInit(): void {
+    this.map = new mapboxgl.Map({
+      container: this.mapContainer.nativeElement,
       style: 'mapbox://styles/mapbox/streets-v11',
       center: [-57.53212015934875, -38.02606601755031],
       zoom: 17,
     });
+  }
+
+  zoomIn() {
+    this.map.zoomIn();
+    console.log('zoomIn', this.mapContainer);
+  }
+  zoomOut() {
+    this.map.zoomOut();
+    console.log('zoomOut', this.mapContainer);
   }
 
 }
