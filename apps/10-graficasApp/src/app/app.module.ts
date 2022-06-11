27f750ffeaa095fa796dc546b00655e734d706bd
jeremyandes <jeremyandes25@gmail.com>
2022-06-11 10:55:49 -0300

    330. Inicio de proyecto - GraficasApp | Plus > PrimeNG

---
+++ apps/10-graficasApp/src/app/app.module.ts
@@ -0,0 +1,20 @@
+import { AppRoutingModule } from './app-routing.module';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { BrowserModule } from '@angular/platform-browser';
+import { NgModule } from '@angular/core';
+
+import { AppComponent } from './app.component';
+
+@NgModule({
+  declarations: [
+    AppComponent
+  ],
+  imports: [
+    BrowserAnimationsModule,
+    BrowserModule,
+    AppRoutingModule
+  ],
+  providers: [],
+  bootstrap: [AppComponent]
+})
+export class AppModule { }
