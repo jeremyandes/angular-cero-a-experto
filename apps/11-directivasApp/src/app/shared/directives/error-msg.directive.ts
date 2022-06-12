796818cdd983cc51f364e2aab1d254e4715d10aa
jeremyandes <jeremyandes25@gmail.com>
2022-06-12 12:26:16 -0300

    350. Cambiar el mensaje de la etiqueta

--- apps/11-directivasApp/src/app/shared/directives/error-msg.directive.ts
+++ apps/11-directivasApp/src/app/shared/directives/error-msg.directive.ts
@@ -6,6 +6,7 @@ import { Directive, ElementRef, Input, OnInit } from '@angular/core';
 export class ErrorMsgDirective implements OnInit {
   htmlElement: ElementRef<HTMLElement>;
   @Input() color: string = 'text-danger';
+  @Input() mensaje: string = 'Campo requerido';
 
   constructor(private el: ElementRef<HTMLElement>) {
     this.htmlElement = el;
@@ -13,9 +14,16 @@ export class ErrorMsgDirective implements OnInit {
 
   ngOnInit(): void {
     this.setColor();
+    this.setMensaje();
   }
 
   setColor(): void {
-    this.htmlElement.nativeElement.classList.add(this.color);
+    const el: HTMLElement = this.htmlElement.nativeElement;
+    el.classList.add(this.color);
+    el.classList.add('form-text');
+  }
+
+  setMensaje(): void {
+    this.htmlElement.nativeElement.innerText = this.mensaje;
   }
 }
