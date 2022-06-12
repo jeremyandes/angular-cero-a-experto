465a01743f570359a3e07799448c7094a19265c3
jeremyandes <jeremyandes25@gmail.com>
2022-06-12 13:17:18 -0300

    355. Directivas estructurales personalizadas

--- apps/11-directivasApp/src/app/shared/shared.module.ts
+++ apps/11-directivasApp/src/app/shared/shared.module.ts
@@ -2,13 +2,15 @@ import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { FormPropertiesComponent } from './components/form-properties/form-properties.component';
 import { ErrorMsgDirective } from './directives/error-msg.directive';
+import { CustomIfDirective } from './directives/custom-if.directive';
 
 
 
 @NgModule({
   declarations: [
     FormPropertiesComponent,
-    ErrorMsgDirective
+    ErrorMsgDirective,
+    CustomIfDirective,
   ],
   imports: [
     CommonModule
@@ -16,6 +18,7 @@ import { ErrorMsgDirective } from './directives/error-msg.directive';
   exports: [
     FormPropertiesComponent,
     ErrorMsgDirective,
+    CustomIfDirective,
   ]
 })
 export class SharedModule { }
