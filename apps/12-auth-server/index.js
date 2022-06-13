5022a02adab7b44d17abc53d2cd0a9285dc2d41b
jeremyandes <jeremyandes25@gmail.com>
2022-06-13 12:16:01 -0300

    363. Configurar servidor de Express

--- apps/12-auth-server/index.js
+++ apps/12-auth-server/index.js
@@ -1,6 +1,16 @@
+const express = require('express');
 
+const app = express();
 
+app.get('/', (req, res) => {
+    res.status(200).json({
+        body: {
+            returnCode: 'OK',
+            message: 'Todo salió bien',
+        }
+    })
+});
 
-
-
-console.log('Hola desde Node');
+app.listen(4000, () => {
+    console.log(`Servidor corriendo en puerto ${process.env.PORT || 4000}`);
+});
