153555657cf32ade5c98dc5c6ef4116385ba8421
jeremyandes <jeremyandes25@gmail.com>
2022-06-13 21:05:10 -0300

    375. Crear usuario en base de datos

--- apps/12-auth-server/controllers/auth.controller.js
+++ apps/12-auth-server/controllers/auth.controller.js
@@ -1,13 +1,47 @@
 const { request, response } = require('express');
+const User = require('../models/User.model');
 
-const newUser = (req = request, res = response) => {
+const newUser = async (req = request, res = response) => {
     const { name, email, password } = req.body;
     console.log(name, email, password);
 
-    return res.json({
-        ok: true,
-        message: 'Crear usuario /new',
-    });
+    try {
+
+        // Verificar email
+        const user = await User.findOne({ email });
+        if (user) {
+            return res.status(400).json({
+                ok: false,
+                message: 'Ya existe un usuario con ese email',
+            })
+        }
+
+        // Crear un usuario con el modelo
+        const dbUser = new User(req.body);
+
+        // Encriptar el password
+
+        // Generar el JWT
+
+        // Crear usuario en la DB
+        await dbUser.save();
+
+        // Generar respuesta exitosa
+        return res.status(201).json({
+            ok: true,
+            uid: dbUser.id,
+            name,
+            email,
+        });
+
+    } catch (error) {
+        console.error(error);
+        return res.status(500).json({
+            ok: false,
+            message: 'Algo salio mal, hable con el administrador',
+        });
+    }
+
 }
 
 const loginUser = (req = request, res = response) => {
