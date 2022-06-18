1e94beb3c08aa83772654394ce48a5e119e201a3
jeremyandes <jeremyandes25@gmail.com>
2022-06-17 21:47:58 -0300

    416. Boton flotante y Logo de Angular

---
+++ apps/13-mapasAppPro/src/app/maps/components/btn-my-location/btn-my-location.component.ts
@@ -0,0 +1,18 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-btn-my-location',
+  templateUrl: './btn-my-location.component.html',
+  styleUrls: ['./btn-my-location.component.scss']
+})
+export class BtnMyLocationComponent {
+
+  constructor(
+
+  ) { }
+
+  goToMyLocation() {
+
+  }
+
+}
