705d12dbebe7c8a3936fa01b90d7ae27e02d2853
jeremyandes <jeremyandes25@gmail.com>
2022-06-17 21:39:58 -0300

    414. Mostrar un mapa de Mapbox

--- apps/13-mapasAppPro/src/app/maps/components/map-view/map-view.component.ts
+++ apps/13-mapasAppPro/src/app/maps/components/map-view/map-view.component.ts
@@ -1,19 +1,28 @@
-import { Component, OnInit } from '@angular/core';
+import { AfterViewInit, Component, ElementRef, ViewChild } from '@angular/core';
 import { PlacesService } from '../../services';
+import MapboxGL from 'mapbox-gl';
 
 @Component({
   selector: 'app-map-view',
   templateUrl: './map-view.component.html',
   styleUrls: ['./map-view.component.scss']
 })
-export class MapViewComponent implements OnInit {
+export class MapViewComponent implements AfterViewInit {
+  @ViewChild('mapContainer') map!: ElementRef;
 
   constructor(
     private placesService: PlacesService,
   ) { }
 
-  ngOnInit(): void {
-    console.log(this.placesService.useLocation);
+  ngAfterViewInit(): void {
+    if (!this.placesService.useLocation) { throw new Error('UseLocation error'); }
+
+    const map = new MapboxGL.Map({
+      container: this.map.nativeElement,
+      style: 'mapbox://styles/mapbox/streets-v11',
+      center: this.placesService.useLocation,
+      zoom: 9
+    });
   }
 
 }
